14ec3e2e46febd0a4ad6434ed2b9f865e5ca73a5
a1 <353658617@qq.com>
2016-03-03 17:10:23 +0800

    first

---
+++ test/test1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#classmate的用法实例
+
+# class A:
+#     #类方法可以被类和类实例调用
+#     @classmethod
+#     def go(self): 
+#         print "类方法能被类和实例调用"
+# A.go()
+#  
+# a=A()
+#  a.go()
+
+
+
+
+# class B:
+#      #实例方法只能被实例调用 
+#     def run(self): 
+#         print "实例方法只能被实例调用"
+#  B.run()
+
+#  b=B() 
+# b.run()
---
+++ test/test2.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*- 
+
+#__init__方法 使用实例  
+class Person():
+    def __init__(self,name):
+        self.name=name
+    def sayhi(self):
+        print("Hello,my name is.%s"%(self.name))
+
+
+p=Person("Swaroop")
+p.sayHi()
+
+
+
+
+def sayHi(self,name): 
+print "Hello, my name is, %s"%(name)  
+p = Person() p.sayHi("hehe")      
+
+
+
+#占位符输出
+ num=10
+ print "a=%d,b=%o,c=%x" %(num,num,num)
+
+
+     class NumberCounter(): 
+    number=0 # #
+    def __init__(self): 
+       NumberCounter.number += 1
+
+# m1=NumberCounter()
+ # print NumberCounter.number
+ # m1.__init__() # print NumberCounter.number
+
+
+
+#     class A(): 
+#     def __init__(self):
+ #         print "一个实例输出" # a=A() # a.__init__()
+
+
+#    class B(): 
+#  def t(self): 
+#  print "一个实例输出" b=B() b.t() 
