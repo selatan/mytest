5eef371913909d056c9c7bf4a848617f2d4778a6
a1 <353658617@qq.com>
2017-01-05 20:14:58 +0800

    python基础

--- test/test3.py
+++ test/test3.py
@@ -9,15 +9,23 @@ print 4567+0x12fd2
 #字符串输出使用""或''扩住,不做计算直接输出
 print("Learn Python in imooc")
 print 'Hello,World'
-print"Hello World"
+print "Hello World"
 
 print(100<99)
 
 print(0xff==255)
 
 #print语句也可以跟上多个字符串，用逗号“,”隔开，就可以连成一串输出
-print'我的名字','是','黄丽娟'
+print '我的名字','是','黄丽娟'
 
 
 print "hello,python"
 print 'hello,','python'
+
+#求等差数列1 4 7 10……前100项的和
+x1 = 1
+d = 3
+n = 100
+x100 = x1 + (n-1)*d
+s = n*(x1+x100)/2
+print s
