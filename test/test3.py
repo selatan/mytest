236a97c240a9edb0800609e4541a6a05dc951bdc
a1 <353658617@qq.com>
2017-01-05 19:55:45 +0800

    python基础

--- test/test3.py
+++ test/test3.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
-#-*- UTF-8 -*-
+# -*- coding: utf-8 -*-
 
-#python中的数据类型
-#
+
+#python的数据类型包括整数\浮点数\字符串\布尔值\空值None
 print(4567+0x12fd2)
+print 4567+0x12fd2
 
+#字符串输出使用""或''扩住,不做计算直接输出
 print("Learn Python in imooc")
+print 'Hello,World'
+print"Hello World"
 
 print(100<99)
 
 print(0xff==255)
+
+#print语句也可以跟上多个字符串，用逗号“,”隔开，就可以连成一串输出
+print'我的名字','是','黄丽娟'
+
+
+print "hello,python"
+print 'hello,','python'
