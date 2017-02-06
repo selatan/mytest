92b68819f3cc8a6d85f659b91b8b0df3fcef3c37
a1 <353658617@qq.com>
2017-02-06 18:05:45 +0800

    python基础

--- test/test4.py
+++ test/test4.py
@@ -76,12 +76,68 @@ import math
 #         break
 # print sum
 
-#利用while True无线循环配合break语句,计算1+2+4+8+…的前20项的和
-sum = 0
-x = 1
-while True:
-    sum = sum+x
-    x = x*2
-    if x>pow(2,19):
-        break
-print sum
+
+# #利用while True无线循环配合break语句,计算1+2+4+8+…的前20项的和
+# sum = 0
+# x = 1
+# while True:
+#     sum = sum+x
+#     x = x*2
+#     if x>pow(2,19):
+#         break
+# print sum
+
+
+# #统计及格分数的平均分
+# L = [75,98,59,81,66,43,69,85]
+# sum = 0.0
+# n = 0
+# for x in L:
+#     if x < 60:
+#         continue
+#     sum = sum +x
+#     n = n +1       #统计及格分数的个数,即循环执行了多少次
+# print sum/n
+
+# #计算1-100内奇数的和
+# sum = 0
+# x = 0
+# while True:
+#     x = x +1
+#     if x >100:
+#         break
+#     if (x%2)==0:
+#         continue
+#     sum = sum + x
+# print sum
+
+
+# for x in ['A','B','C']:
+#     for y in ['1','2','3']:
+#         print (x+y)
+
+
+# #使用两重循环打印出100以内所有十位数数字比个位数数字小的数
+# for x in [1,2,3,4,5,6,7,8,9]:
+#     for y in [0,1,2,3,4,5,6,7,8,9]:
+#         if x<y:
+#             print(x*10+y)
+
+
+
+# d = {
+#     'Adam': 95,
+#     'Lisa': 85,
+#     'Bart': 59
+# }
+# print d['Adam']
+# print d['Lisa']
+# print d['Bart']
+
+d = {
+    'Adam':95,
+    'Lisa':85,
+    'Bart':59
+}
+for key in d:
+    print key +':',d[key]
